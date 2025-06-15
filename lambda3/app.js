b6b36446c564e1464835b82c7177755993c6ae1e
Thavasumoorthi <thavasumoorthi@gmail.com>
2025-06-15 07:14:47 +0530

    added

--- lambda3/app.js
+++ lambda3/app.js
@@ -1,2 +1,3 @@
 console.log("app is located under the lambda3 function")
 console.log("added some line")
+
--- lambda3/index.js
+++ lambda3/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 
 const app=express()
 
-
+console.log("added soem files")
 
 app.listen(5000,()=>{
     console.log("app is   ")
