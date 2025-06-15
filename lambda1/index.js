2a1380dcf0a21d8b7d8aec19f7ea3de6ac1aa9d8
Thavasumoorthi <thavasumoorthi@gmail.com>
2025-06-15 09:14:23 +0530

    removed health check in each folder

--- lambda1/index.js
+++ lambda1/index.js
@@ -3,10 +3,6 @@ const express=require('express')
 const app=express()
 
 
-app.get("/health",(req,res)=>{
-      res.json({message:"health are passed"})
-})
-
 
 app.listen(5000,()=>{
     console.log("app is running on 5000 ")
--- lambda2/index.js
+++ lambda2/index.js
@@ -3,10 +3,7 @@ const express=require('express')
 const app=express()
 
 
-app.get("/health",(req,res)=>{
-    console.log("health are passed")
-      res.json({message:"health are passed"})
-})
+
 
 app.listen(3000,()=>{
     console.log("app is running on 3000 ")
