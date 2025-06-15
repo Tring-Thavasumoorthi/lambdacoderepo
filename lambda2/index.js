b194bceb8d5c169dc714924bc716d957344b0e39
Thavasumoorthi <thavasumoorthi@gmail.com>
2025-06-15 08:38:15 +0530

    added health in the index file

--- lambda2/index.js
+++ lambda2/index.js
@@ -3,6 +3,9 @@ const express=require('express')
 const app=express()
 
 
+app.get("/health",(req,res)=>{
+      res.json({message:"health are passed"})
+})
 
 app.listen(3000,()=>{
     console.log("app is running on 3000 ")
